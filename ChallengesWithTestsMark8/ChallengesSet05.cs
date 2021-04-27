ad59faa992eeecfd4223f37a76007ffb2bcdeb67
Jeffrey Baker <jeffrey1871@gmail.com>
2021-04-26 20:34:06 -0500

    Wk5 Update

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -21,22 +22,46 @@ namespace ChallengesWithTestsMark8
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            foreach (Business item in businesses)
+            {
+                if (item.TotalRevenue == 0)
+                    item.Name = "CLOSED";
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+                return false;
+            for (int i = 0; i < numbers.Length - 1; i++)
+                if (numbers[i] > numbers[i + 1])
+                    return false;
+            return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            var total = 0;
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+            for (int i = 1; i < numbers.Length; i++)
+                if (numbers[i - 1] % 2 == 0)
+                    total += numbers[i];
+            return total;
+
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0)
+                return "";
+            string ans = "";
+            foreach(string word in words)
+                if(string.IsNullOrWhiteSpace(word) == false)
+                   ans += word.Trim(' ') + " ";
+            if (ans.Length == 0)
+                return "";
+            return ans.TrimEnd(' ') + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
